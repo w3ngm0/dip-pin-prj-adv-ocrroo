c6be654736994e768a6deaf18fe8b8ee07192f63
w3ngm0 <kabywangmo0808@gmail.com>
2025-10-28 13:54:34 +0800

    feat: save video frames

--- preliminary/library_basics.py
+++ preliminary/library_basics.py
@@ -12,25 +12,27 @@ Make sure you read the docstrings C.A.R.E.F.U.L.Y (yes, I took the L to check th
 """
 
 # imports - add all required imports here
-from pathlib import Path
+
 import cv2
+from pathlib import Path
 import numpy as np
+from PIL import Image
 
 
-VID_PATH = Path("resources/name-of-vid-given-to-you-by-instructor.mp4")
+VID_PATH = Path("../resources/oop.mp4")
+OUT_PATH = Path("../resources/")
 
 class CodingVideo:
     capture: cv2.VideoCapture
 
-
     def __init__(self, video: Path | str):
-        self.capture = ... # You complete me!
+        self.capture = cv2.VideoCapture(video)  # You complete me!
         if not self.capture.isOpened():
             raise ValueError(f"Cannot open {video}")
 
-        self.fps = ...
-        self.frame_count = ...
-        self.duration = ...
+        self.fps = self.capture.get(cv2.CAP_PROP_FPS)
+        self.frame_count = self.capture.get(cv2.CAP_PROP_FRAME_COUNT)
+        self.duration = self.frame_count/self.fps  # duration = frame_count/fps
 
 
     def __str__(self) -> str:
@@ -45,12 +47,18 @@ class CodingVideo:
         ----------
         https://docs.opencv.org/3.4/d4/d15/group__videoio__flags__base.html#gaeb8dd9c89c10a5c63c139bf7c4f5704d
         """
+        info_string = (f"Current video has the following prop: " +
+                       f'{self.fps:.2f} fps' + "\n"
+                       f'{self.frame_count:.2f} counts per frame' + '\n'
+                       f'{self.duration / 60:.2f} minutes')
+
+        return info_string
 
     def get_frame_number_at_time(self, seconds: int) -> int:
         """Given a time in seconds, returns the value of the nearest frame"""
+        return int(seconds * self.fps)
 
-
-    def get_frame_rgb_array(self, frame_number: int) -> np.ndarray:
+    def get_frame_rgb_array(self,capture: cv2.VideoCapture, frame_number: int) -> np.ndarray:
         """Returns a numpy N-dimensional array (ndarray)
 
         The array represents the RGB values of each pixel in a given frame
@@ -62,6 +70,12 @@ class CodingVideo:
         # TODO: Find a tutorial on OpenCV that demonstrates color space conversion
 
         """
+        capture.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+        ret, frame = capture.read()
+        if not ret:
+            raise ValueError(f"Invalid read {frame_number}")
+        return cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
 
     def get_image_as_bytes(self, seconds: int) -> bytes:
         self.capture.set(cv2.CAP_PROP_POS_FRAMES, self.get_frame_number_at_time(seconds))
@@ -77,18 +91,32 @@ class CodingVideo:
 
 
     def save_as_image(self, seconds: int, output_path: Path | str = 'output.png') -> None:
-      """Saves the given frame as a png image
+        """Saves the given frame as a png image
 
-      # TODO: Requires a third-party library to convert ndarray to png
-      # TODO: Identify the library and add a reference to its documentation
+        # TODO: Requires a third-party library to convert ndarray to png
+        # TODO: Identify the library and add a reference to its documentation
 
 
-      """
+        """
+        if type(output_path) is str:
+            output_path = OUT_PATH/output_path
+
+        frame = self.get_frame_number_at_time(seconds)
+        self.capture.set(cv2.CAP_PROP_POS_FRAMES, frame-1)
+        ok, frame = self.capture.read()
+        if not ok:
+            raise ValueError("Unable to read frame from file")
+
+        image = Image.fromarray(frame)
+        image.save(output_path)
+
+
 def test():
     """Try out your class here"""
-    oop = CodingVideo("resources/oop.mp4")
+    oop = CodingVideo(VID_PATH)
     print(oop)
     oop.save_as_image(42)
+
 
 if __name__ == '__main__':
     test()
